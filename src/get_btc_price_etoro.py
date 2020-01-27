bb49a1a691e56f4ecdc5ebb1891bd2061d267a6c
pohanwww <pohanwww@gmail.com>
2020-01-27 22:54:56 +0800

    get bitcon history data

--- src/get_btc_price_etoro.py
+++ src/get_btc_price_etoro.py
@@ -13,17 +13,20 @@ etoro_password = config['ETORO']['PASSWORD']
 
 options = Options()
 # options.add_argument("--headless")            #不開啟實體瀏覽器背景執行
-options.add_argument("--start-maximized")     #最大化視窗
+# options.add_argument("--start-maximized")     #最大化視窗
 options.add_argument("--incognito")           #開啟無痕模式
 driver = webdriver.Firefox(firefox_options=options)
 
 #login etoro account
 driver.get("https://www.etoro.com/login")
 time.sleep(1)
-account_input = driver.find_elements_by_css_selector("input#username")
-account_input[0].send_keys(etoro_account)
-password_input = driver.find_elements_by_css_selector("input#password")
-password_input[0].send_keys(etoro_password)
+account_input = driver.find_element_by_css_selector("input#username")
+account_input.send_keys(etoro_account)
+password_input = driver.find_element_by_css_selector("input#password")
+password_input.send_keys(etoro_password)
+sign_in_btn = driver.find_elements_by_css_selector("div.w-login-btn-wrapp")
+
+sign_in_btn[-1].click()
 time.sleep(1)
 
 # login_btn = driver.find_elements_by_css_selector("button#login-sts-btn-sign-in")
