77fc1b27e462bad9670e32a957908807a7a7a83c
pohanwww <pohanwww@gmail.com>
2020-03-03 08:42:00 +0800

    quary etoro history price

--- src/test_api_request.py
+++ src/test_api_request.py
@@ -1,19 +1,52 @@
 import requests
 import pandas as pd
+import time
 
 instrument_id_list = ['17', '18', '19', '22']
+# instrument_id_list = ['19'] 
+# time_mode_list = ['TenMinutes']
 time_mode_list = ['OneMinute', 'FiveMinutes', 'TenMinutes', 'FifteenMinutes', 'ThirtyMinutes', 'OneHour', 'FourHours', 'OneDay', 'OneWeek']
-etoro = requests.get('https://candle.etoro.com/candles/desc.json/'+time_mode_list[0]+'/1000/' + instrument_id_list[0]) #natgas
+# etoro = requests.get('https://candle.etoro.com/candles/desc.json/'+time_mode_list[0]+'/1000/' + instrument_id_list[0]) #natgas
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/OneDay/1000/17') #oil
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/OneDay/1000/18') #gold
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/OneDay/1000/19') #silver
-content = etoro.json()
-# print(content['Candles'][0]['Candles'])
-df = pd.DataFrame(content['Candles'][0]['Candles'])
-try:
-    df_old = pd.read_csv('./etoro_data/' + instrument_id_list[0] + '_' + time_mode_list[0] + '.csv')
-    # df_old['FromDate'][0]
-except:
-    print('there is no data of {}, {}'.format(instrument_id_list[0], time_mode_list[0]))
-    df.to_csv('./etoro_data/' + instrument_id_list[0] + '_' + time_mode_list[0] + '.csv')
-print(df['FromDate'][0])
+
+for instrument_id in instrument_id_list:
+    for time_mode in time_mode_list:
+        try:
+            etoro = requests.get('https://candle.etoro.com/candles/desc.json/'+time_mode+'/1000/' + instrument_id) #natgas
+            time.sleep(0.5)
+            content = etoro.json()
+            df_new = pd.DataFrame(content['Candles'][0]['Candles'])
+            try:
+                df_old = pd.read_csv('./etoro_data/' + instrument_id + '_' + time_mode + '.csv')
+                print('--')
+                print(time_mode, instrument_id)
+                # print(df_new[df_new['FromDate'] == df_old['FromDate'][0]].index[0])
+                try:
+                    print('hi',df_old['FromDate'][0])
+                    print(df_new.head())
+                    print(df_new[df_new['FromDate'] == df_old['FromDate'][0]])
+                    index_ = df_new[df_new['FromDate'] == df_old['FromDate'][0]].index[0]
+                    print('--')
+                    print(index_)
+                    df_old_drop = df_old.drop(index=0, axis=0)
+                    print('1')
+                    index_ = index_ + 1
+                    print('2')
+                    # print(df_new.drop(df_old.index[index_:]).count())
+                    
+                    # print(df_old_drop.count())
+                    df_new_drop = df_new.drop(index=[index_,], axis=0)
+                    print('3')
+                    new = pd.concat([df_new_drop,df_old_drop],axis=0, ignore_index = True)
+                    # print(new.count())
+                    
+                    new.to_csv('./etoro_data/' + instrument_id + '_' + time_mode + '.csv', index=False)
+                except:
+                    print('process error')
+            except:
+                print('There is no data of {}, {}'.format(instrument_id, time_mode))
+                df_new.to_csv('./etoro_data/' + instrument_id + '_' + time_mode + '.csv', index=False)
+        except:
+            print('api error')
