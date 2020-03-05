58551c35a826717f7dd5883bb538cf0c7fe288cc
pohanwww <pohanwww@gmail.com>
2020-03-06 02:13:55 +0800

    fix the df.drop error and update the etoro data

--- src/Etoro_data_collection.py
+++ src/Etoro_data_collection.py
@@ -3,9 +3,10 @@ import pandas as pd
 import time
 
 instrument_id_list = ['17', '18', '19', '22']
-# instrument_id_list = ['19'] 
-# time_mode_list = ['TenMinutes']
 time_mode_list = ['OneMinute', 'FiveMinutes', 'TenMinutes', 'FifteenMinutes', 'ThirtyMinutes', 'OneHour', 'FourHours', 'OneDay', 'OneWeek']
+# instrument_id_list = ['17'] 
+# time_mode_list = ['OneWeek']
+
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/'+time_mode_list[0]+'/1000/' + instrument_id_list[0]) #natgas
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/OneDay/1000/17') #oil
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/OneDay/1000/18') #gold
@@ -26,7 +27,7 @@ for instrument_id in instrument_id_list:
                     df_old_drop = df_old.drop(index=0, axis=0)
                     index_ = index_ + 1
                     # print(df_new.drop(df_old.index[index_:]).count())
-                    df_new_drop = df_new.drop(index=[index_,], axis=0)
+                    df_new_drop = df_new.drop(df_new.index[index_:], axis=0)
                     new = pd.concat([df_new_drop,df_old_drop],axis=0, ignore_index = True)
                     new.to_csv('./etoro_data/' + instrument_id + '_' + time_mode + '.csv', index=False)
                 except:
