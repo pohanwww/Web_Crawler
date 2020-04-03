1c67cce576f01bf8a3caf2e40895c0776280357a
Po-Han Wu <pohanwww@gmail.com>
2020-04-03 15:57:14 +0800

    update code

--- src/Etoro_data_collection.py
+++ src/Etoro_data_collection.py
@@ -2,10 +2,10 @@ import requests
 import pandas as pd
 import time
 
-instrument_id_list = ['17', '18', '19', '22']
-time_mode_list = ['OneMinute', 'FiveMinutes', 'TenMinutes', 'FifteenMinutes', 'ThirtyMinutes', 'OneHour', 'FourHours', 'OneDay', 'OneWeek']
-# instrument_id_list = ['17'] 
-# time_mode_list = ['OneWeek']
+# instrument_id_list = ['17', '18', '19', '22']
+# time_mode_list = ['OneMinute', 'FiveMinutes', 'TenMinutes', 'FifteenMinutes', 'ThirtyMinutes', 'OneHour', 'FourHours', 'OneDay', 'OneWeek']
+instrument_id_list = ['17'] 
+time_mode_list = ['FiveMinutes']
 
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/'+time_mode_list[0]+'/1000/' + instrument_id_list[0]) #natgas
 # etoro = requests.get('https://candle.etoro.com/candles/desc.json/OneDay/1000/17') #oil
@@ -20,7 +20,7 @@ for instrument_id in instrument_id_list:
             content = etoro.json()
             df_new = pd.DataFrame(content['Candles'][0]['Candles'])
             try:
-                df_old = pd.read_csv('./etoro_data/' + instrument_id + '_' + time_mode + '.csv')
+                df_old = pd.read_csv('./etoro_data/' + instrument_id + '_current' + time_mode + '.csv')
                 try:
                     # print(df_new[df_new['FromDate'] == df_old['FromDate'][0]])
                     index_ = df_new[df_new['FromDate'] == df_old['FromDate'][0]].index[0]
@@ -34,6 +34,6 @@ for instrument_id in instrument_id_list:
                     print('process error {}, {}'.format(instrument_id, time_mode))
             except:
                 print('There is no data of {}, {}'.format(instrument_id, time_mode))
-                # df_new.to_csv('./etoro_data/' + instrument_id + '_' + time_mode + '.csv', index=False)
+                df_new.to_csv('./etoro_data/' + instrument_id + '_current' + time_mode + '.csv', index=False)
         except:
             print('api error')
